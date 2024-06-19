f943c4977e6c330471a69e73472f3be5e96d67ed
Yash_Varpe <yashvarpe2005@gmail.com>
2024-06-19 14:45:21 +0530

    added the vercel json

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require("express");
-const socket = require("socket.io");
-const http = require("http");
-const { Chess } = require("chess.js");
-const path = require("path");
+import express from "express";
+import { Server as SocketServer } from "socket.io";
+import http from "http";
+import { Chess } from "chess.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const server = http.createServer(app);
-const io = socket(server);
+const io = new SocketServer(server);
 
 const chess = new Chess();
 let players = {};
 let currentPlayer = "W";
 
 app.set("view engine", "ejs");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
